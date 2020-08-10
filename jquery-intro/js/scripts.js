1496a25634f4acdd3b741122ed61b47f667c1dad
David McCaslin <davidmccaslin94@gmail.com>
2020-08-10 14:07:22 -0700

    added initial file structure. created html page and linked jquery + custom script file to it

---
+++ jquery-intro/js/scripts.js
@@ -0,0 +1,13 @@
+
+jQuery("h1").click(function() {
+  alert("this is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("this is a paragraph.");
+});
+
+jquery("img").click(function() {
+  alert("This is an image.");
+});
+
